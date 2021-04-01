bb13aa0041f159f0b4623bde4870d62f156026b9
Csaba Peter <peter_csaba@yahoo.com>
2021-04-01 13:42:32 +1100

    killall players

--- tv_on.sh
+++ tv_on.sh
@@ -1,5 +1,6 @@
 #start playing the video
 killall -9 omxplayer
+killall -9 omxplayer.bin
 omxplayer --loop -o hdmi /home/pi/videolooper/video/fireplace.mp4&
 echo 'on 0.0.0.0' | cec-client -s -d 1
 #echo 'standby 0.0.0.0' | cec-client -s -d 1
